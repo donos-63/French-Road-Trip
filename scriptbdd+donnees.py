13cf64db8a9dae16ed017129989690585458a95e
mathieu <mn.simon@live.fr>
2020-07-21 18:37:31 +0200

    refonte accés bdd + initialisation

--- scriptbdd+donnees.py
+++ scriptbdd+donnees.py
@@ -2,80 +2,121 @@
 # # Import des différentes bibliothèques 
 
 import sys
-import sqlite3  # C'est un provider (fournisseur de service) terme svt utilisé pour les bases de données
 import os   # Le module os est un module  fournit par Python dont le but d’interagir avec le système d’exploitation, il permet ainsi de gérer l’arborescence des fichiers, de fournir des informations sur le système d’exploitation processus, variables systèmes, ainsi que de nombreuses fonctionnalités du systèmes…
 import csv   # Import de la fonction permettant de travailler ac des fichiers CSV (writer, reader)
+import psycopg2 #provider bdd postgres
+import urllib.parse as up
+import collections
+from zipfile import ZipFile
 
-# AFFECTATION DES VARIABLES
+#Définition des chemins vers le référentiel des villes
+CITY_REFERENTIAL_FOLDER = os.getcwd() + os.sep + "city_referential"  + os.sep 
+CITY_REFERENTIAL_IN_PATH = CITY_REFERENTIAL_FOLDER + "city_referential.zip"
+CITY_REFERENTIAL_OUT_PATH = CITY_REFERENTIAL_FOLDER + "city_referential.csv"
 
-DB_PATH = os.getcwd() + os.sep + "db_sncf.db" 
-# DB_PATH = "C:\Users\utilisateur\Documents\2. PROJETS\PROJET 1 SNCF vacances ac Mathieu" + "\db_sncf.db"      chemin dur ne fonctionne pas
+#dezip du référentiel de donnée pour intégration à la base de donnée
+if not os.path.exists(CITY_REFERENTIAL_OUT_PATH):
+    if not os.path.exists(CITY_REFERENTIAL_IN_PATH):
+        print("Le fichier de référentiel des villes n'existe pas, import impossible")
+        exit
+    else :
+        with ZipFile(CITY_REFERENTIAL_IN_PATH, 'r') as zipObj:
+            zipObj.extractall(CITY_REFERENTIAL_FOLDER)
+            print('Référentiel des villes extrait')
 
-# SI LA BDD EXISTE ALORS ON LA SUPPRIME, SUPPRESSION DE LA BDD EXISTANTE
-if os.path.exists(DB_PATH):
-        bdd = sqlite3.connect(DB_PATH)
-        bdd.close()
-        os.remove(DB_PATH)
 
-# CONNECTION A LA BDD + AJOUT SI ELLE N'EXISTE PAS
-db_sncf = sqlite3.connect(DB_PATH)
-cur = db_sncf.cursor()
+#Initialisation du provider de donnée
+up.uses_netloc.append("postgres")
+#todo mettre l'url dans les variables d'environnement (venv/pypi)
+url = up.urlparse('postgres://herdlqqw:5GkFSWjQSp1Plap4gGnE-kX6mY7J1QB9@packy.db.elephantsql.com:5432/herdlqqw')
+conn = psycopg2.connect(database=url.path[1:],
+user=url.username,
+password=url.password,
+host=url.hostname,
+port=url.port
+)
 
-# # CREATION TABLES
-cur.execute('''CREATE TABLE prefecture (insee_code INTEGER PRIMARY KEY, postal_code INTEGER,
-city TEXT, department INTEGER, region TEXT, statut TEXT, average_altitude FLOAT, area FLOAT,
-population FLOAT, geo_lat FLOAT, geo_long FLOAT, id_geo TEXT, common_code INTEGER, canton_code INTEGER,
-district_code INTEGER, departement_code INTEGER, area_code INTEGER);''')
-print("Création de la table prefecture effectuée.")
+cur = conn.cursor()
 
-cur.execute('''CREATE TABLE journey (insee_code_from INTEGER, isee_code_to INTEGER, co2 FLOAT, duration INTEGER);''')
-print("Création de la table journey effectuée.")
+# CREATION TABLES
+cur.execute("SELECT to_regclass('prefecture');")
+isTableExists = cur.fetchone()[0] == None
+
+if isTableExists :
+    cur.execute('''CREATE TABLE prefecture (insee_code INTEGER PRIMARY KEY, postal_code TEXT,
+    city TEXT, department TEXT, region TEXT, statut TEXT, average_altitude FLOAT, area_size FLOAT,
+    population FLOAT, geo_lat FLOAT, geo_long FLOAT, city_code INTEGER, canton_code INTEGER,
+    district_code INTEGER, departement_code INTEGER, area_code INTEGER);''')
+    print("Création de la table prefecture effectuée.")
+else :
+    print("La table 'prefecture' existe déjà")
+
+cur.execute("SELECT to_regclass('journey');")
+isTableExists = cur.fetchone()[0] == None
+
+if isTableExists :
+    cur.execute('''CREATE TABLE journey (insee_code_from INTEGER, isee_code_to INTEGER, co2 FLOAT, duration INTEGER);''')
+    print("Création de la table journey effectuée.")
+else :
+    print("La table 'journey' existe déjà")
 
 # # ## SAUVEGARDE DE LA BDD
-db_sncf.commit()
-print("BDD sauvegardée.")
+#pas besoin sur un create
+conn.commit()
+#print("BDD sauvegardée.")
 
-# with open('bdd_sncf.csv', newline='') as csvfile:   #  Si csvfile est un fichier, il doit être ouvert avec newline=''
-#     bdd_sncf = csv.reader(csvfile, delimiter=' ', quotechar='|')
-#     i = 0
-#     for row in bdd_sncf:
-#         print(i)
-#         str.split (";")
-#         i = i + 1
-
-# print("Fin de l'écriture de la BDD")
-
-
-with open('bdd_sncf.csv', mode="r", encoding="utf-8") as file :
-    dic = csv.DictReader(file)
+with open(CITY_REFERENTIAL_OUT_PATH, mode="r", encoding="utf-8") as file :
+    dic = csv.DictReader(file, delimiter = ';')
     for row in dic :
-        if not row["statut"].endswith("Préfecture"):
+        #la ville n'est pas une préfecture on passe à la ligne suivante
+        if not row["Statut"].startswith("Préfecture") and not row["Statut"].startswith("Capital"): 
             continue
-        v1 = row["insee_code"]
-        v2 = row["postal_code"]
-        v3 = row["city"]
-        v4 = row["department"]
-        v5 = row["region"]
-        v6 = row["statut"]
-        v7 = row["average_altitude"]
-        v8 = row["area"]
-        v9 = row["population"]
-        v10 = row["geo_lat"]
-        v11 = row["geo_long"]
-        v12 = row["id_geo"]
-        v13 = row["common_code"]
-        v14 = row["canton_code"]
-        v15 = row["district_code"]
-        v16 = row["departement_code"]
-        v17 = row["area_code"]
 
+        #si la ville n'est pas en france métropolitaine (DOM et Corse) on passe à la ligne suivante
+        if not row["Code INSEE"].isnumeric() or int(row["Code INSEE"]) >= 96000: 
+            continue
 
+        values = [ row["Code INSEE"],
+                   row["Code Postal"],
+                   row["Commune"],
+                   row["Département"],
+                   row["Région"],
+                   row["Statut"],
+                   row["Altitude Moyenne"],
+                   row["Superficie"],
+                   row["Population"],
+                   row["geo_point_2d"].split(',')[0],
+                   row["geo_point_2d"].split(',')[1],
+                   row["Code Commune"],
+                   row["Code Canton"],
+                   row["Code Arrondissement"],
+                   row["Code Département"],
+                   row["Code Région"]
+                ]
 
-        variable = [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17]
-        cur.execute("""INSERT OR IGNORE 
-        INTO prefecture(insee_code, postal_code, city, department,region, statut, average_altitude, area,
-        population, geo_lat, geo_long, id_geo, common_code, canton_code, district_code, departement_code, area_code) 
-        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);""", variable)
-    db_sncf.commit()
+        cur.execute("""INSERT 
+                        INTO prefecture (insee_code, 
+                                        postal_code, 
+                                        city, 
+                                        department,
+                                        region, 
+                                        statut, 
+                                        average_altitude, 
+                                        area_size, 
+                                        population, 
+                                        geo_lat, 
+                                        geo_long, 
+                                        city_code, 
+                                        canton_code, 
+                                        district_code, 
+                                        departement_code, 
+                                        area_code) 
+                        VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                        ON CONFLICT DO NOTHING
+                        ;""", values)
+
+    conn.commit()
+    
+    print('Base de donnée initialisée')
 
 
